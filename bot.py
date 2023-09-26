0951991950f44b9d63ebb00c9c639f5f4d577cfe
William K. Black <william.kevin.black@gmail.com>
2023-09-26 21:53:12 +0000

    random imports; issue w/ numpy :/

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 # 👉 Run "./connect" (or "connect.cmd" on Windows) in the terminal to get started
+
+import numpy as np
+
 class Bot:
     def __init__(self, config):
         print("Hello World!", config)
@@ -10,7 +13,8 @@ class Bot:
         # Return the spot you'd like to move here.
         # 1st value: x, should be an integer between 0 and 2
         # 2nd value: y, should be an integer between 0 and 2
-        return (0, 1)
+        x,y = np.random.randint(0,3,2)
+        return x,y
 
     def end(self, board):
         print("Good game!")
